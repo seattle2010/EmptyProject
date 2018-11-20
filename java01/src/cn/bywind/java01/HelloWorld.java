aa4d1dc37a23f473f0de6fcce103e6c1a55bd7d1
seattlemac <seattlemac@seattlemacdeiMac.local>
2018-11-20 23:07:05 +0800

    Initial commit

---
+++ java01/src/cn/bywind/java01/HelloWorld.java
@@ -0,0 +1,12 @@
+package cn.bywind.java01;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("123");
+		init();
+	}
+
+	private static void init() {
+		Test test = new Object();
+	}
+}
